8c770e5563f0f6db3f156e61be8799ed0431d88d
Debian LibreOffice Maintainers <debian-openoffice@lists.debian.org>
2012-08-01 08:41:17 +0000

    gtk3-hack-explicit-gmodule
    For whatever reason we get undefined referenced to g_module_*.
    
    
    Gbp-Pq: Name gtk3-hack-explicit-gmodule.diff

--- vcl/Library_vclplug_gtk3.mk
+++ vcl/Library_vclplug_gtk3.mk
@@ -55,6 +55,7 @@ $(eval $(call gb_Library_add_api,vclplug_gtk3,\
 $(eval $(call gb_Library_add_libs,vclplug_gtk3,\
     $$(GTK3_LIBS) \
     $$(GTHREAD_LIBS) \
+    $$(GMODULE_LIBS) \
 ))
 
 $(eval $(call gb_Library_add_linked_libs,vclplug_gtk3,\
