04098ec5bbe87c1669f01f4fc1b04ac04013d38d
Caolán McNamara <caolanm@redhat.com>
2014-09-10 14:41:42 +0100

    clip PolyPolygon::Insert on .svm load
    valgrind + bff
    
    (cherry picked from commit fd64d444b730f6cb7216dac8f6e3f94b97d7ab60)
    
    Conflicts:
    	tools/source/generic/poly2.cxx
    
    Change-Id: Ib3ed8a6e518c0686f8cbeaf021b9ca3a07005032
    Reviewed-on: https://gerrit.libreoffice.org/11382
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>
    
    
    Gbp-Pq: Name CVE-2016-1513.diff

--- tools/source/generic/poly2.cxx
+++ tools/source/generic/poly2.cxx
@@ -219,7 +219,7 @@ void PolyPolygon::Insert( const Polygon& rPoly, sal_uInt16 nPos )
 void PolyPolygon::Remove( sal_uInt16 nPos )
 {
     DBG_CHKTHIS( PolyPolygon, NULL );
-    DBG_ASSERT( nPos < Count(), "PolyPolygon::Remove(): nPos >= nSize" );
+    assert(nPos < Count() && "PolyPolygon::Remove(): nPos >= nSize");
 
     if ( mpImplPolyPolygon->mnRefCount > 1 )
     {
@@ -239,7 +239,7 @@ void PolyPolygon::Remove( sal_uInt16 nPos )
 void PolyPolygon::Replace( const Polygon& rPoly, sal_uInt16 nPos )
 {
     DBG_CHKTHIS( PolyPolygon, NULL );
-    DBG_ASSERT( nPos < Count(), "PolyPolygon::Replace(): nPos >= nSize" );
+    assert(nPos < Count() && "PolyPolygon::Replace(): nPos >= nSize");
 
     if ( mpImplPolyPolygon->mnRefCount > 1 )
     {
@@ -256,7 +256,7 @@ void PolyPolygon::Replace( const Polygon& rPoly, sal_uInt16 nPos )
 const Polygon& PolyPolygon::GetObject( sal_uInt16 nPos ) const
 {
     DBG_CHKTHIS( PolyPolygon, NULL );
-    DBG_ASSERT( nPos < Count(), "PolyPolygon::GetObject(): nPos >= nSize" );
+    assert(nPos < Count() && "PolyPolygon::GetObject(): nPos >= nSize");
 
     return *(mpImplPolyPolygon->mpPolyAry[nPos]);
 }
@@ -617,7 +617,7 @@ Rectangle PolyPolygon::GetBoundRect() const
 Polygon& PolyPolygon::operator[]( sal_uInt16 nPos )
 {
     DBG_CHKTHIS( PolyPolygon, NULL );
-    DBG_ASSERT( nPos < Count(), "PolyPolygon::[](): nPos >= nSize" );
+    assert(nPos < Count() && "PolyPolygon::[](): nPos >= nSize");
 
     if ( mpImplPolyPolygon->mnRefCount > 1 )
     {
