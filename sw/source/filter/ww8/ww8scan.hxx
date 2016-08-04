LibreOffice/core

convert pStatus to vector and use at to check offsets
Change-Id: I5186f6a65bb9d5ed8a0d1ab1d71f7e2c13865411

sw/source/filter/ww8/ww8scan.hxx
@@ -721,8 +721,8 @@ class WW8PLCFx_Book : public WW8PLCFx
 {
 private:
     WW8PLCFspecial* pBook[2];           // Start and End Position
-    ::std::vector<OUString> aBookNames;   // Name
-    eBookStatus* pStatus;
+    std::vector<OUString> aBookNames;   // Name
+    std::vector<eBookStatus> aStatus;
     long nIMax;                         // Number of Booknotes
     sal_uInt16 nIsEnd;
     sal_Int32 nBookmarkId; // counter incremented by GetUniqueBookmarkName.

raspbian-packages/libreoffice

(cherry picked from commit ea70088895ed45dc60abf18319acc1b4fa3018dd)
Reviewed-on: https://gerrit.libreoffice.org/17695
Reviewed-by: David Tardon <dtardon@redhat.com>
Tested-by: David Tardon <dtardon@redhat.com>
Gbp-Pq: Name pStatus-vector-offsets.diff

sw/source/filter/ww8/ww8scan.hxx
@@ -703,8 +703,8 @@ class WW8PLCFx_Book : public WW8PLCFx
 {
 private:
     WW8PLCFspecial* pBook[2];           // Start and End Position
-    ::std::vector<String> aBookNames;   // Name
-    eBookStatus* pStatus;
+    std::vector<String> aBookNames;   // Name
+    std::vector<eBookStatus> aStatus;
     long nIMax;                         // Number of Booknotes
     sal_uInt16 nIsEnd;
     int nBookmarkId; // counter incremented by GetUniqueBookmarkName.