4fb2254fabe8d773519768ea968d2f7f37a9eefd
Debian LibreOffice Maintainers <debian-openoffice@lists.debian.org>
2017-11-06 06:07:31 +0000

    sensible-lomua
    ===================================================================
    
    Gbp-Pq: Name sensible-lomua.diff

--- shell/source/unix/misc/senddoc.sh
+++ shell/source/unix/misc/senddoc.sh
@@ -77,6 +77,30 @@ if [ "$1" = "--mailclient" ]; then
     shift
 fi
 
+case `basename "$MAILER"` in
+    sensible-lomua)
+        if [ -n "$KDE_FULL_SESSION" -a -x /usr/bin/kde-open ] \
+           || [ -x /usr/bin/gnome-open ] \
+           || [ -x /usr/bin/xdg-open ]; then
+            # use an undefined mailer, to trigger the default handling
+            MAILER=undefined
+        elif [ -n "$GNOME_DESKTOP_SESSION_ID" -a -x /usr/bin/evolution ]; then
+            MAILER=/usr/bin/evolution
+        elif [ -n "$KDE_FULL_SESSION" -a -x /usr/bin/kmail ]; then
+            MAILER=/usr/bin/kmail
+        elif [ -x /usr/bin/evolution ]; then
+            # default
+            MAILER=/usr/bin/evolution
+        elif [ -x /usr/bin/icedove ]; then
+            # fallback
+            MAILER=/usr/bin/icedove
+        elif [ -x /usr/bin/thunderbird ]; then
+            # fallback
+            MAILER=/usr/bin/thunderbird
+        fi
+        ;;
+esac
+
 # autodetect mail client from executable name
 case `basename "$MAILER" | sed 's/-.*$//'` in
 
